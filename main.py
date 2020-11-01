5e62dba54da4daeb5c5a7a4320ce332a28b63bb5
Lukas Schwab <lukas.schwab@gmail.com>
2020-10-31 22:20:29 -0700

    Configure for use as Cloud Function
    Update to use jsonfeed-wrapper 1.0.0

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import jsonfeed as jf
 from bs4 import BeautifulSoup as bs
 
 BASE_URL = "https://atlasofplaces.com"
+BASE_URL_FORMAT = BASE_URL + "/{category}"
 MAX_ITEMS = 20
 
 def toItem(link):
@@ -19,9 +20,14 @@ def toItem(link):
         tags=text
     )
 
-def page_to_items(page):
-    soup = bs(page.text, 'html.parser')
+def page_to_items(response):
+    soup = bs(response.text, 'html.parser')
     links = soup.findAll("section", class_="overview")[0].findAll("a")
     return [toItem(l) for l in links[:MAX_ITEMS]]
 
-app = jfw.initialize("Atlas of Places", BASE_URL, page_to_items, MAX_ITEMS)
+wrapper = jfw.JSONFeedWrapper("Atlas of Places", BASE_URL_FORMAT, page_to_items, MAX_ITEMS)
+
+# App for App Engine deployments.
+app = wrapper.as_bottle_app()
+# Target for Cloud Function deployments.
+target = wrapper.as_cloud_function()
